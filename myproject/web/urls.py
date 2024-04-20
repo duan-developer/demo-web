3a03b99c948438b25e0b2f2b96d775e08895d656
duan-developer <trananhduan2k4@gmail.com>
2024-04-20 21:15:08 +0700

    Code Moi

--- myproject/web/urls.py
+++ myproject/web/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 urlpatterns = [
     path('', views.Nhap),
-    
+    path('login/', views.LOG),
+    path('dangky/', views.REG),
 ]
--- myproject/web/views.py
+++ myproject/web/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def Nhap(request): 
     return render(request,'home.html')
+def LOG(request): 
+    return render(request,'login.html')
+def REG(request): 
+    return render(request,'register.html')
 
